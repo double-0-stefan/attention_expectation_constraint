8200107a3b04b0ca3b7200cf5737735411b2e6a5
Stefan Brugger <stefpb@gmail.com>
2023-10-02 19:30:10 +0100

    auto-updated

---
+++ update_git.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#rm -r dist
+#python3 setup.py sdist bdist_wheel
+git add .
+git commit -m 'auto-updated'
+git push https://double-0-stefan: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@github.com/double-0-stefan/attention_expectation_constraint.git
+
+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
